a6bcea21f89b6744ce8286d00e57d76bd32091a9
劉洧熏 <terry@gamer.com.tw>
2024-11-16 15:40:52 +0800

    - 修改：調整 GridView UI

--- BadmintonCall_SwiftUI/View/GridView.swift
+++ BadmintonCall_SwiftUI/View/GridView.swift
@@ -123,4 +123,81 @@ struct GridCell: View {
             }
         }
     }
+    
+    var playerInfoContent: some View {
+        VStack {
+            Text(playerInfo.name)
+                .font(.headline)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            Divider()
+                .background(.gray.opacity(0.1))
+            
+            HStack {
+                Text(playerInfo.level.nameTag)
+                    .font(.caption2)
+                    .fontWeight(.bold)
+                    .padding(EdgeInsets(top: 2, leading: 4, bottom: 2, trailing: 4))
+                    .cornerRadius(3)
+                    .background(Color.black.opacity(0.1))
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 3, style: .circular)
+                            .stroke(Color.white, lineWidth: 0.5)
+                    }
+                Text("上場次數： \(playerInfo.playTimes)" )
+                    .font(.caption)
+                    .fontWeight(.thin)
+                    .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+            }
+            .padding(EdgeInsets(top: 4, leading: 0, bottom: 0, trailing: 0))
+        }
+        .padding()
+        .foregroundColor(.white)
+        .background(playerInfo.level.color)
+        .cornerRadius(8)
+        .onTapGesture {
+            onItemTapped(playerInfo)
+        }
+        .onLongPressGesture(minimumDuration: 2.0) {
+            onItemLongPress(playerInfo)
+        }
+    }
+    
+    var playerStateView: some View {
+        VStack {
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("隊友")
+                        .padding()
+                        .font(.subheadline)
+                        .foregroundColor(.black)
+                }
+
+                Menu {
+                    ForEach(dataModel.players) { player in
+                        Button {
+                            dataModel.setupPartner(for: playerInfo, by: player.id.uuidString)
+                        } label: {
+                            Text(player.name)
+                        }
+                    }
+                } label: {
+                    Text("球友名單")
+                }
+            }
+            Text("Popup View")
+                .padding()
+                .background(Color.yellow)
+                .cornerRadius(10)
+                .frame(width: 300, height: 200)
+        }
+        .onTapGesture {
+            withAnimation {
+                self.showPopover = false
+            }
+        }
+    }
 }
+
